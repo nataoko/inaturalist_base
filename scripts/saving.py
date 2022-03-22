1d1667837d650ccbead041229d526b5fbb203418
Natalia Okopna <natalia.okopna@gmail.com>
2022-03-22 11:42:41 +0100

    Function to generate new data file added.

--- scripts/saving.py
+++ scripts/saving.py
@@ -3,6 +3,11 @@ import os
 import shutil
 
 
+def generate_new_data_file():
+    data = {"areas": {}, "observations": []}
+    with open('data'+os.sep+'data.json', 'w') as outfile:
+        outfile.write(json.dumps(data))
+
 
 def open_data():
     shutil.copyfile('data'+os.sep+'data.json', 'data'+os.sep+'data_bk.json')
