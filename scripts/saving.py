12480eaf5bab7a94ce1083f8e08dd9f1dd5cdb5f
Natalia Okopna <natalia.okopna@gmail.com>
2022-03-29 13:27:01 +0200

    refactoring(I guess)

--- scripts/saving.py
+++ scripts/saving.py
@@ -14,14 +14,6 @@ def open_data():
     return json.load(open('data'+os.sep+'data.json'))
 
 
-def valid_name(name, data):
-    if not name.isalnum():
-        return 1
-    if name in data['areas']:
-        return 2
-    return name
-
-
 def save_new_area(name, locs, data):
     data['areas'][name] = locs
     with open('data'+os.sep+'data.json', 'w') as outfile:
